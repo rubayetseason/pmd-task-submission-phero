10f89b0d03628d57b4ba2313ec976b737fde351e
rubayet-islam <season.ss2001@gmail.com>
2024-04-30 22:30:06 +0600

    zustand for task management

--- src/zustand/projectStore.ts
+++ src/zustand/projectStore.ts
@@ -1,15 +1,26 @@
-import { ProjectType } from "@/types/types";
+import { ProjectType, TaskType } from "@/types/types";
 import { create } from "zustand";
 
 type ProjectStoreType = {
   projects: ProjectType[];
+  singleProject: ProjectType | null;
   addProject: (newProjects: ProjectType[]) => void;
   updateProject: (updatedProject: ProjectType) => void;
   deleteProject: (projectId: string) => void;
+  setSingleProject: (project: ProjectType | null) => void;
+  addTask: (task: TaskType) => void;
+  deleteTask: (taskId: string) => void;
+  assignTaskMembers: (taskId: string, members: string[]) => void;
+  editTask: (updatedTask: TaskType) => void;
+  markTaskStatus: (
+    taskId: string,
+    newStatus: "incomplete" | "in progress" | "done"
+  ) => void;
 };
 
 export const useProjectStore = create<ProjectStoreType>((set) => ({
   projects: [],
+  singleProject: null,
   addProject(newProjects: ProjectType[]) {
     set({ projects: newProjects });
   },
@@ -25,4 +36,84 @@ export const useProjectStore = create<ProjectStoreType>((set) => ({
       projects: state.projects.filter((project) => project.id !== projectId),
     }));
   },
+  setSingleProject: (project: ProjectType | null) =>
+    set({ singleProject: project }),
+
+  addTask: (task: TaskType) =>
+    set((state) => {
+      if (state.singleProject) {
+        return {
+          singleProject: {
+            ...state.singleProject,
+            tasks: [...state.singleProject.tasks, task],
+          },
+        };
+      }
+      return {};
+    }),
+  deleteTask: (taskId: string) =>
+    set((state) => {
+      if (state.singleProject) {
+        return {
+          singleProject: {
+            ...state.singleProject,
+            tasks: state.singleProject.tasks.filter(
+              (task) => task.id !== taskId
+            ),
+          },
+        };
+      }
+      return {};
+    }),
+
+  editTask: (updatedTask: TaskType) =>
+    set((state) => {
+      if (state.singleProject) {
+        const updatedTasks = state.singleProject.tasks.map((task) =>
+          task.id === updatedTask.id ? updatedTask : task
+        );
+        return {
+          singleProject: {
+            ...state.singleProject,
+            tasks: updatedTasks,
+          },
+        };
+      }
+      return {};
+    }),
+
+  assignTaskMembers: (taskId: string, members: string[]) =>
+    set((state) => {
+      if (state.singleProject) {
+        const updatedTasks = state.singleProject.tasks.map((task) =>
+          task.id === taskId ? { ...task, assignedMembers: members } : task
+        );
+        return {
+          singleProject: {
+            ...state.singleProject,
+            tasks: updatedTasks,
+          },
+        };
+      }
+      return {}; // No action if no project selected
+    }),
+
+  markTaskStatus: (
+    taskId: string,
+    newStatus: "incomplete" | "in progress" | "done"
+  ) =>
+    set((state) => {
+      if (state.singleProject) {
+        const updatedTasks = state.singleProject.tasks.map((task) =>
+          task.id === taskId ? { ...task, status: newStatus } : task
+        );
+        return {
+          singleProject: {
+            ...state.singleProject,
+            tasks: updatedTasks,
+          },
+        };
+      }
+      return {};
+    }),
 }));
