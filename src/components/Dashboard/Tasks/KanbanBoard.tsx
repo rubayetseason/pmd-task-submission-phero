74abad0656824ce531ae63856fffce62a7879e40
rubayet-islam <season.ss2001@gmail.com>
2024-05-01 10:21:09 +0600

    FEAT: adjusted kanban board according to tasktype

--- src/components/Dashboard/Tasks/KanbanBoard.tsx
+++ src/components/Dashboard/Tasks/KanbanBoard.tsx
@@ -10,7 +10,7 @@ import React, {
 import { FiPlus, FiTrash } from "react-icons/fi";
 import { motion } from "framer-motion";
 import { FaFire } from "react-icons/fa";
-import { ProjectType } from "@/types/types";
+import { ProjectType, TaskType } from "@/types/types";
 
 interface ITaskProps {
   projectData: ProjectType;
@@ -25,29 +25,29 @@ export const KanbanBoard: React.FC<ITaskProps> = ({ projectData }) => {
 };
 
 const Board: React.FC<ITaskProps> = ({ projectData }) => {
-  console.log(projectData);
+  const tasksData = projectData?.tasks;
 
-  const [cards, setCards] = useState(DEFAULT_CARDS);
+  const [cards, setCards] = useState(tasksData);
 
   return (
     <div className="h-full w-full px-5 py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 overflow-scroll">
       <Column
         title="TODO"
-        column="todo"
+        status="incomplete"
         headingColor="text-yellow-200"
         cards={cards}
         setCards={setCards}
       />
       <Column
         title="In progress"
-        column="doing"
+        status="in progress"
         headingColor="text-blue-200"
         cards={cards}
         setCards={setCards}
       />
       <Column
         title="Complete"
-        column="done"
+        status="done"
         headingColor="text-emerald-200"
         cards={cards}
         setCards={setCards}
@@ -60,21 +60,21 @@ const Board: React.FC<ITaskProps> = ({ projectData }) => {
 type ColumnProps = {
   title: string;
   headingColor: string;
-  cards: CardType[];
-  column: ColumnType;
-  setCards: Dispatch<SetStateAction<CardType[]>>;
+  cards: TaskType[];
+  status: ColumnType;
+  setCards: Dispatch<SetStateAction<TaskType[]>>;
 };
 
 const Column = ({
   title,
   headingColor,
   cards,
-  column,
+  status,
   setCards,
 }: ColumnProps) => {
   const [active, setActive] = useState(false);
 
-  const handleDragStart = (e: DragEvent, card: CardType) => {
+  const handleDragStart = (e: DragEvent, card: TaskType) => {
     e.dataTransfer.setData("cardId", card.id);
   };
 
@@ -94,7 +94,7 @@ const Column = ({
 
       let cardToTransfer = copy.find((c) => c.id === cardId);
       if (!cardToTransfer) return;
-      cardToTransfer = { ...cardToTransfer, column };
+      cardToTransfer = { ...cardToTransfer, status };
 
       copy = copy.filter((c) => c.id !== cardId);
 
@@ -165,7 +165,7 @@ const Column = ({
   const getIndicators = () => {
     return Array.from(
       document.querySelectorAll(
-        `[data-column="${column}"]`
+        `[data-column="${status}"]`
       ) as unknown as HTMLElement[]
     );
   };
@@ -175,7 +175,7 @@ const Column = ({
     setActive(false);
   };
 
-  const filteredCards = cards.filter((c) => c.column === column);
+  const filteredCards = cards.filter((c) => c.status === status);
 
   return (
     <div className="w-full px-2 md:px-1 shrink-0">
@@ -196,29 +196,29 @@ const Column = ({
         {filteredCards.map((c) => {
           return <Card key={c.id} {...c} handleDragStart={handleDragStart} />;
         })}
-        <DropIndicator beforeId={null} column={column} />
-        <AddCard column={column} setCards={setCards} />
+        <DropIndicator beforeId={null} column={status} />
+        <AddCard column={status} setCards={setCards} />
       </div>
     </div>
   );
 };
 
-type CardProps = CardType & {
+type CardProps = TaskType & {
   handleDragStart: Function;
 };
 
-const Card = ({ title, id, column, handleDragStart }: CardProps) => {
+const Card = ({ taskName, id, status, handleDragStart }: CardProps) => {
   return (
     <>
-      <DropIndicator beforeId={id} column={column} />
+      <DropIndicator beforeId={id} column={status} />
       <motion.div
         layout
         layoutId={id}
         draggable="true"
-        onDragStart={(e) => handleDragStart(e, { title, id, column })}
+        onDragStart={(e) => handleDragStart(e, { taskName, id, status })}
         className="cursor-grab rounded border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing"
       >
-        <p className="text-sm text-neutral-100">{title}</p>
+        <p className="text-sm text-neutral-100">{taskName}</p>
       </motion.div>
     </>
   );
@@ -242,7 +242,7 @@ const DropIndicator = ({ beforeId, column }: DropIndicatorProps) => {
 const BurnBarrel = ({
   setCards,
 }: {
-  setCards: Dispatch<SetStateAction<CardType[]>>;
+  setCards: Dispatch<SetStateAction<TaskType[]>>;
 }) => {
   const [active, setActive] = useState(false);
 
@@ -281,7 +281,7 @@ const BurnBarrel = ({
 
 type AddCardProps = {
   column: ColumnType;
-  setCards: Dispatch<SetStateAction<CardType[]>>;
+  setCards: Dispatch<SetStateAction<TaskType[]>>;
 };
 
 const AddCard = ({ column, setCards }: AddCardProps) => {
@@ -344,35 +344,4 @@ const AddCard = ({ column, setCards }: AddCardProps) => {
   );
 };
 
-type ColumnType = "backlog" | "todo" | "doing" | "done";
-
-type CardType = {
-  title: string;
-  id: string;
-  column: ColumnType;
-};
-
-const DEFAULT_CARDS: CardType[] = [
-  // TODO
-  {
-    title: "Research DB options for new microservice",
-    id: "5",
-    column: "todo",
-  },
-  { title: "Postmortem for outage", id: "6", column: "todo" },
-  { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
-
-  // DOING
-  {
-    title: "Refactor context providers to use Zustand",
-    id: "8",
-    column: "doing",
-  },
-  { title: "Add logging to daily CRON", id: "9", column: "doing" },
-  // DONE
-  {
-    title: "Set up DD dashboards for Lambda listener",
-    id: "10",
-    column: "done",
-  },
-];
+type ColumnType = "incomplete" | "in progress" | "done";
